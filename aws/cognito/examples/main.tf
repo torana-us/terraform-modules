b04fd09d899fcd43d92d13f5064958c9c12b95dc
Wataru Kurashima <what.r.j150@gmail.com>
2023-06-23 17:17:43 +0900

    docs: ✏️ add example

---
+++ aws/cognito/examples/main.tf
@@ -0,0 +1,16 @@
+module "cognito" {
+  source = "../"
+
+  name             = "example"
+  callback_urls    = ["https://example.com/oauth2/idpresponse"]
+  client_id        = "example_id"
+  client_secret    = "example_secret"
+  user_pool_domain = "example-${random_string.example.result}"
+}
+
+resource "random_string" "example" {
+  length = 10
+  special = false
+  numeric = true
+  min_numeric = 10
+}
