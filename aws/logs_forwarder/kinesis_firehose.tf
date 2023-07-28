426461b6bf148042de683c7004d18323a7f3b211
Wataru Kurashima <what.r.j150@gmail.com>
2023-07-28 10:25:11 +0900

    fix: 🐛 validate

--- aws/logs_forwarder/kinesis_firehose.tf
+++ aws/logs_forwarder/kinesis_firehose.tf
@@ -51,12 +51,10 @@ resource "aws_kinesis_firehose_delivery_stream" "datadog_logs_forwarder" {
     }
   }
 
-
-
   lifecycle {
     ignore_changes = [
       http_endpoint_configuration[0].access_key,
-      http_endpoint_configuration[0].request_configuration.*.common_attributes
+      http_endpoint_configuration[0].request_configuration[0]
     ]
   }
 }
