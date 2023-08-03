e5a02bfc76fec00299bf681754864b411b61962a
Wataru Kurashima <what.r.j150@gmail.com>
2023-08-03 09:32:26 +0900

    fix: 🐛 vpc need ipv4_cidr_block

--- aws/subnet/examples/ipv6_only/main.tf
+++ aws/subnet/examples/ipv6_only/main.tf
@@ -14,23 +14,28 @@ module "subnet" {
 }
 
 locals {
+  cidr_block = "192.168.0.0/16"
   subnets = {
     subnet-1a_1 = {
       az              = "ap-northeast-1a"
+      cidr_block      = cidrsubnet(local.cidr_block, 8, 1)
       ipv6_cidr_block = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 1)
     }
     subnet-1c_1 = {
       az              = "ap-northeast-1c"
+      cidr_block      = cidrsubnet(local.cidr_block, 8, 2)
       ipv6_cidr_block = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 2)
     }
     subnet-1d_1 = {
       az              = "ap-northeast-1d"
+      cidr_block      = cidrsubnet(local.cidr_block, 8, 3)
       ipv6_cidr_block = cidrsubnet(aws_vpc.vpc.ipv6_cidr_block, 8, 3)
     }
   }
 }
 
 resource "aws_vpc" "vpc" {
+  cidr_block                       = local.cidr_block
   assign_generated_ipv6_cidr_block = true
 }
 
